769b9a318fe81494deb6591c3d2ad4fa3fe710b3
Richion <48645031+Richion@users.noreply.github.com>
2020-11-10 01:14:40 +0800

    Create .env

--- index.js
+++ index.js
@@ -219,26 +219,48 @@ app.listen(process.env.PORT||80,function(){
  
 */
 
-var linebot = require('linebot');
-var express = require('express');
+'use strict';
+const result = require('dotenv').config();
+if (result.error) throw result.error
 
-var bot = linebot({
-  channelId: '1655189233',
-  channelSecret: 'b6657925a6344cdae0f0cd9263b16142',
-  channelAccessToken: 'VgWXWX4q7uA4WjGaZPM47YUDkABofwitC5b1aa0rGVw4F2YOdc7fS0Oy0bk8LDi88uuZVCHAR2kVWJ4rjYBcCZGarHx+yZQrdvIEJsp3wAdPYkydhDrm4YSFnPrse/wbJBZUXW/RqIZxbX55i+yWTAdB04t89/1O/w1cDnyilFU='
+const linebot = require('linebot');
+const Express = require('express');
+const BodyParser = require('body-parser');
+
+// Line Channel info
+const bot = linebot({
+  channelId: process.env.LINE_CHANNEL_ID,
+  channelSecret: process.env.LIEN_CHANNEL_SECRET,
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
 });
 
-
-bot.on('message', function(event) {
-  console.log(event); //把收到訊息的 event 印出來看看
-});
-
-const app = express();
 const linebotParser = bot.parser();
-app.post('/', linebotParser);
+const app = Express();
+// for line webhook usage
+app.post('/linewebhook', linebotParser);
 
-//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-var server = app.listen(process.env.PORT || 8080, function() {
-  var port = server.address().port;
-  console.log("App now running on port", port);
+app.use(BodyParser.urlencoded({ extended: true }));
+app.use(BodyParser.json());
+
+// a http endpoint for trigger broadcast
+app.post('/broadcast', (req, res) => {
+  bot.broadcast(req.body.message).then(() => {
+    res.send('broadcast ok');
+  }).catch(function (error) {
+    res.send('broadcast fail');
+  });
+});
+
+app.listen(3000);
+
+// echo user message
+bot.on('message', function (event) {
+  // get user message from `event.message.text`
+  // reply same message
+  var replyMsg = `${event.message.text}`;
+  event.reply(replyMsg).then(function (data) {
+    console.log('ok')
+  }).catch(function (error) {
+    console.error(error)
+  });
 });
