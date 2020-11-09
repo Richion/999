35b09e05c3dbd61ad8f2cae09f0720d2f0995705
Richion <48645031+Richion@users.noreply.github.com>
2020-11-09 16:04:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var linebot = require('linebot');
 var request = require('request');
 
 var cheerio = require('cheerio');
-const express = require('express');
-const app = express();	
 
-//const linebotParser = bot.parser();
+
+
 
 
 var url = 'https://oursogo.com/forum-174-1.html';
@@ -80,7 +79,7 @@ var bot = linebot({
   channelAccessToken: 'VgWXWX4q7uA4WjGaZPM47YUDkABofwitC5b1aa0rGVw4F2YOdc7fS0Oy0bk8LDi88uuZVCHAR2kVWJ4rjYBcCZGarHx+yZQrdvIEJsp3wAdPYkydhDrm4YSFnPrse/wbJBZUXW/RqIZxbX55i+yWTAdB04t89/1O/w1cDnyilFU='
 });
 
-bot.on('message',function (event) { 
+var linebotParser = bot.on('message',function (event) { 
 	if(event.message.text==="抽"){
 		demo(function(req,img){
 			x = Math.floor(Math.random()*img.length);
@@ -198,14 +197,10 @@ bot.on('message', function (event) {
 
 // Bot所監聽的webhook路徑與port
 
-//app.post('/webhook', linebotParser);
 
-bot.listen('/linewebhook', 3000, function () {
+
+bot.listen('/linewebhook', 8080, function () {
     console.log('[BOT已準備就緒]');
 });
-/*
-const server = app.listen(process.env.PORT || 8080, function() {
-    let port = server.address().port;
-    console.log('目前的port是', port);
-});
-*/
+
+
